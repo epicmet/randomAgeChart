78396f2ebff8154e5cf95532bdc8dc567fafb3df
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-23 16:18:59 +0330

    get data from api and sort them by age

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { getRandomNumber, sortByAge } from "./utils";
+
+const url = "https://randomuser.me/api/";
 
 const App = () => {
-  return <div>Hello world</div>;
+  const [people, setPeople] = useState([]);
+  const [min, setMin] = useState(null);
+  const [max, setMax] = useState(null);
+
+  useEffect(async () => {
+    const resp = await fetch(`${url}?results=${getRandomNumber()}`);
+    const data = await resp.json();
+    console.log(data);
+    const sortedArr = sortByAge(data.results);
+    setPeople(sortedArr);
+    setMin(sortedArr[0].dob.age);
+    setMax(sortedArr[sortedArr.length - 1].dob.age);
+  }, []);
+
+  return <div></div>;
 };
 
 export default App;
