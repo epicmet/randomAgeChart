67ecd5a3ed1ab5bacdbc415dc5fa7933d1095820
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-25 10:36:54 +0330

    made isEmptyArray and hasChanged function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,7 @@
 export const getRandomNumber = (min = 10, max = 100) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+export const isEmptyArray = (arr) => arr.length === 0;
+
+export const hasChanged = (initial, current) => initial !== current;
